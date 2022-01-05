8406cc3b9fde5f0507d836e87ce8412b9a950e6a
Robert Kim <68884194+robertkim1@users.noreply.github.com>
2022-01-04 22:47:06 -0500

    added ticker entry

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/piquette/finance-go/quote"
 	"optionsCalculator/blackScholes"
 	"strconv"
 )
@@ -13,6 +15,7 @@ import (
 type numericalEntry struct {
 	widget.Entry
 }
+
 func newNumericalEntry() *numericalEntry {
 	entry := &numericalEntry{}
 	entry.ExtendBaseWidget(entry)
@@ -37,6 +40,65 @@ func (e *numericalEntry) TypedShortcut(shortcut fyne.Shortcut) {
 	}
 }
 
+func appBuilder() {
+	myApp := app.New()
+	mainWindow := myApp.NewWindow("Options Calculator")
+	mainWindow.Resize(fyne.NewSize(600, 600))
+	myApp.Settings().SetTheme(theme.LightTheme())
+
+	typeOfOptionLabel := widget.NewLabel("Type of Option")
+	var optionTypeString string
+	optionTypeRead := widget.NewSelect([]string{"Call Option", "Put Option"}, func(value string) {
+		optionTypeString = value
+	})
+	equityLabel := widget.NewLabel("Ticker Symbol")
+	ticker := widget.NewEntry()
+	ticker.SetPlaceHolder("Ticker Symbol")
+	spotPriceLabel := widget.NewLabel("Current Share Price")
+	spotPrice := newNumericalEntry()
+	spotPrice.SetPlaceHolder("Enter current share price")
+	strikePriceLabel := widget.NewLabel("Strike Price of Option")
+	strikePrice := newNumericalEntry()
+	strikePrice.SetPlaceHolder("Enter strike price")
+	timeLabel := widget.NewLabel("Time until Expiration in Days")
+	time := newNumericalEntry()
+	time.SetPlaceHolder("Enter time until expiration (days)")
+	volatilityLabel := widget.NewLabel("Volatility")
+	volatility := newNumericalEntry()
+	volatility.SetPlaceHolder("Enter volatility percentage (decimal)")
+	rfRateLabel := widget.NewLabel("Risk-Free Interest Rate")
+	rfRate := newNumericalEntry()
+	rfRate.SetPlaceHolder("Enter current risk-free interest rate")
+	output := widget.NewLabel("Option Price:")
+
+	calculate := widget.NewButton("Calculate", func() {
+		var spotPriceFloat float64
+		if ticker.Text != "" {
+			q, _ := quote.Get(ticker.Text)
+			spotPriceFloat = q.RegularMarketPrice
+		} else {
+			spotPriceFloat, _ = strconv.ParseFloat(spotPrice.Text, 64)
+		}
+		strikePriceFloat, _ := strconv.ParseFloat(strikePrice.Text, 64)
+		timeFloat, _ := strconv.ParseFloat(time.Text, 64)
+		volatilityFloat, _ := strconv.ParseFloat(volatility.Text, 64)
+		rfRateFloat, _ := strconv.ParseFloat(rfRate.Text, 64)
+		d1 := blackScholes.D1(spotPriceFloat, strikePriceFloat, rfRateFloat, volatilityFloat, timeFloat/365)
+		d2 := blackScholes.D2(d1, volatilityFloat, timeFloat/365)
+		if optionTypeString == "Call Option" {
+			callPrice := blackScholes.CallPrice(d1, spotPriceFloat, d2, strikePriceFloat, rfRateFloat, timeFloat/365)
+			output.SetText(fmt.Sprintf("Option Price: $%.2f", callPrice))
+		} else {
+			putPrice := blackScholes.PutPrice(d1, spotPriceFloat, d2, strikePriceFloat, rfRateFloat, timeFloat/365)
+			output.SetText(fmt.Sprintf("Option Price: $%.2f", putPrice))
+		}
+	})
+
+	content := container.NewVBox(typeOfOptionLabel, optionTypeRead, equityLabel, ticker, spotPriceLabel, spotPrice, strikePriceLabel, strikePrice, timeLabel, time, volatilityLabel, volatility, rfRateLabel, rfRate, calculate, output)
+	mainWindow.SetContent(content)
+	mainWindow.ShowAndRun()
+}
+
 func main() {
 
 	//var spotPrice float64 = 97.39
@@ -47,51 +109,5 @@ func main() {
 	//call price output: 5.32
 	//put price output: 4.83
 
-	myApp := app.New()
-	mainWindow := myApp.NewWindow("Options Calculator")
-	mainWindow.Resize(fyne.NewSize(600, 600))
-
-	typeOfOptionLabel := widget.NewLabel("Type of Option")
-	var optionTypeString string
-	optionTypeRead := widget.NewSelect([]string{"Call Option","Put Option"}, func(value string) {
-		optionTypeString = value
-	})
-	spotPriceLabel := widget.NewLabel("Current Share Price")
-	spotPrice := newNumericalEntry()
-	spotPrice.SetPlaceHolder("Enter current share price")
-	strikePriceLabel := widget.NewLabel("Strike Price of Option")
-	strikePrice := newNumericalEntry()
-	strikePrice.SetPlaceHolder("Enter strike price")
-	timeLabel := widget.NewLabel("Time until Expiration in Years")
-	time := newNumericalEntry()
-	time.SetPlaceHolder("Enter time until expiration (years)")
-	volatilityLabel := widget.NewLabel("Volatility")
-	volatility := newNumericalEntry()
-	volatility.SetPlaceHolder("Enter volatility percentage (decimal)")
-	rfRateLabel := widget.NewLabel("Risk-Free Interest Rate")
-	rfRate := newNumericalEntry()
-	rfRate.SetPlaceHolder("Enter current risk-free interest rate")
-	output := widget.NewLabel("")
-
-	calculate := widget.NewButton("Calculate", func() {
-		spotPriceFloat, _ := strconv.ParseFloat(spotPrice.Text, 64)
-		strikePriceFloat, _ := strconv.ParseFloat(strikePrice.Text, 64)
-		timeFloat, _ := strconv.ParseFloat(time.Text, 64)
-		volatilityFloat, _ := strconv.ParseFloat(volatility.Text, 64)
-		rfRateFloat, _ := strconv.ParseFloat(rfRate.Text, 64)
-		d1 := blackScholes.D1(spotPriceFloat, strikePriceFloat, rfRateFloat, volatilityFloat, timeFloat)
-		d2 := blackScholes.D2(d1, volatilityFloat, timeFloat)
-		if optionTypeString == "Call Option" {
-			callPrice := blackScholes.CallPrice(d1, spotPriceFloat, d2, strikePriceFloat, rfRateFloat, timeFloat)
-			output.SetText(fmt.Sprintf("$%.2f", callPrice))
-		} else {
-			putPrice := blackScholes.PutPrice(d1, spotPriceFloat, d2, strikePriceFloat, rfRateFloat, timeFloat)
-			output.SetText(fmt.Sprintf("$%.2f", putPrice))
-		}
-	})
-
-	content := container.NewVBox(typeOfOptionLabel, optionTypeRead, spotPriceLabel, spotPrice,strikePriceLabel, strikePrice, timeLabel, time, volatilityLabel, volatility, rfRateLabel, rfRate, calculate, output)
-	mainWindow.SetContent(content)
-	mainWindow.ShowAndRun()
-
+	appBuilder()
 }
